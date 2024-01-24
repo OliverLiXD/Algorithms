967183235dfae9b068bb1426d70e50ac81e518f9
OliverLiXD <xingyuli1996@gmail.com>
2024-01-24 19:25:05 +0800

    Initialize repository on local

---
+++ DataStructures.h
@@ -0,0 +1,7 @@
+//
+// Created by 64949 on 2024-01-24.
+//
+
+struct TreeNode;
+struct ListNode;
+
---
+++ Sort/Leetcode0148.cpp
@@ -0,0 +1,20 @@
+#include "../DataStructures.h"
+#include "iostream"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        
+    }
+};
